981cd4ba99869540611655729d3e5e97b55bc960
danghuyanh06-cyber <danghuyanh46@gmail.com>
2026-09-25 16:23:51 +0700

    Delete Ex5.py

--- Ex5.py
+++
@@ -1,7 +0,0 @@
-colors = ["Blue", "Red", "Green", "Orange", "White"]
-c = input("What is your favorite color?: ")
-if c in colors:
-    a = colors.index(c)
-    print("Your color is at index", a, "in my list")
-else:
-    print("Sorry, I could not find your color")
